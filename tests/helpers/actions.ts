5ceb59eae2637eb62d128a0f98acc9afd78b608a
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-18 13:03:03 +0500

    helper completes

--- tests/helpers/actions.ts
+++ tests/helpers/actions.ts
@@ -15,5 +15,14 @@ export async function addtocart(page: Page) {
     await page.getByRole('link', { name: 'View Cart' }).click();
 };
 
-
+export async function Checkout(page: Page) {
+    await page.getByText('Proceed To Checkout').click();
+    await page.getByRole('link', { name: 'Place Order'}).click();
+    await page.locator('input[name="name_on_card"]').fill('Agustina Spinka');
+    await page.locator('input[name="card_number"]').fill('2221552724486674');
+    await page.getByRole('textbox', { name: 'ex.'}).fill('734');
+    await page.getByRole('textbox', { name: 'MM'}).fill('09');
+    await page.getByRole('textbox', { name: 'YYYY'}).fill('2025');
+    await page.getByRole('button', { name: 'Pay and Confirm Order'}).click();
+};
 
