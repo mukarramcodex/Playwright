db13d9d9f92bae0d9c166a5ad6b808eb79d188bf
Mukarram Ali <info.mukarramali@gmail.com>
2025-07-29 13:45:29 +0500

    Update Minor

--- playwright-framework/tests/login.spec.ts
+++ playwright-framework/tests/login.spec.ts
@@ -1,6 +1,9 @@
 import { expect, test } from '@playwright/test'
 import { LoginPage } from '../pages/LoginPage';
 import config from '../env/qa.json';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 test('Login with Credentials', async({ page }) => {
     const loginpage = new LoginPage(page);
@@ -15,4 +18,12 @@ test('Test using JSON Config', async({ page }) => {
     await page.fill('#password', config.password);
     await page.click('button[type="submit"]');
     await expect(page).toHaveURL(/dashboard/);
+});
+
+test('Login test using .env', async ({ page }) => {
+  await page.goto(process.env.BASE_URL!);
+  await page.fill('#username', process.env.USERNAME!);
+  await page.fill('#password', process.env.PASSWORD!);
+  await page.click('button[type="submit"]');
+  await expect(page).toHaveURL(/dashboard/);
 });
